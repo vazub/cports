0d6c1c6d030208d778c86221f9a65c7a60ea486a
vazub <vazub@zubko.cc>
2023-04-17 21:40:32 +0200

    initial template draft

---
+++ contrib/emptty/template.py
@@ -0,0 +1,13 @@
+pkgname = "emptty"
+pkgver = "0.9.1"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+hostmakedepends = ["gmake", "go"]
+pkgdesc = "Dead simple CLI Display Manager on TTY"
+maintainer = "vazub <chimera@zubko.cc>"
+license = "MIT"
+url = "https://github.com/tvrzna/emptty"
+source = f"https://github.com/tvrzna/{pkgname}/archive/refs/tags//v{pkgver}.tar.gz"
+sha256 = "1b3c940da8d60705306525bf5a7619a7b30d954679e30a978a03ac53357e57d4"
+
