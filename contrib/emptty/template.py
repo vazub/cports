e37acc6a48881ac150df443ba367c10343685c72
vazub <vazub@zubko.cc>
2023-04-20 22:19:45 +0200

    emptty port

--- contrib/emptty/template.py
+++ contrib/emptty/template.py
@@ -1,13 +1,38 @@
 pkgname = "emptty"
 pkgver = "0.9.1"
 pkgrel = 0
+<<<<<<< Updated upstream
 build_style = "makefile"
 make_cmd = "gmake"
 hostmakedepends = ["gmake", "go"]
+=======
+build_style = "go"
+make_check_args = [
+    "./src",
+]
+hostmakedepends = ["go", "linux-pam-devel", "libx11-devel"]
+>>>>>>> Stashed changes
 pkgdesc = "Dead simple CLI Display Manager on TTY"
 maintainer = "vazub <chimera@zubko.cc>"
 license = "MIT"
 url = "https://github.com/tvrzna/emptty"
 source = f"https://github.com/tvrzna/{pkgname}/archive/refs/tags//v{pkgver}.tar.gz"
 sha256 = "1b3c940da8d60705306525bf5a7619a7b30d954679e30a978a03ac53357e57d4"
+<<<<<<< Updated upstream
 
+=======
+options = ["spdx"]
+
+def post_install(self):
+    self.install_man("res/emptty.1")
+    self.install_dir("etc/emptty")
+    self.install_file("res/conf", "etc/emptty")
+    self.install_file("res/motd-gen.sh", "etc/emptty")
+    self.install_dir("etc/pam.d")
+    self.install_file(
+        src = "res/pam",
+        dest = "etc/pam.d",
+        name = "emptty"
+    )
+    self.install_service("res/dinit-service", "emptty")
+>>>>>>> Stashed changes
